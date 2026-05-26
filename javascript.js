7eac070d36f898918ff290b0dd9f87b4fd22e3ad
codemanic001 <workingnitesh1@gmail.com>
2026-05-26 08:09:59 +0530

    these ar ethe changges made

--- javascript.js
+++ javascript.js
@@ -5,3 +5,28 @@ a=90;
 console.log(a);
 b=60;
 console.log(a+b);
+const max=57;
+const actual=max-13;
+const percentage=actual/max;
+console.log(percentage);
+ let v=true;
+// alert(typeof(v));
+//  v=String(v);
+// alert(typeof(v));
+// alert("6"/"3");
+// let str="123";
+// alert(typeof(str));
+// str=Number(str);
+// alert(typeof(str));
+// let age=Number("Nitesh Singh");
+// alert(age);
+// let test=Number(true);
+// alert(test);
+// alert( Boolean(1) );
+// alert( Boolean(0) );
+// alert(Boolean("0"));
+// alert(Boolean("1"));
+// alert(Boolean(" "));
+alert('1'+2);//if any one is string other is automatically converted to a string
+alert(2+2+'1');//answer is different since first is an integer
+alert('2'+2+1);
