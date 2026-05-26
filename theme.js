a8759d5b2568035212f90e61004f2f651ac44cbd
codemanic001 <workingnitesh1@gmail.com>
2026-05-26 08:07:18 +0530

    This was the example given to us

---
+++ theme.js
@@ -0,0 +1,8 @@
+const select=document.querySelector("select");
+const html=document.querySelector("html");
+document.body.style.padding="10px";
+function update(bgColor,textColor){
+    html.style.backgroundColor=bgColor;
+    html.style.color=textColor;
+}
+select.addEventListener("change",() => select.value==="black"?update("white","black"):update("black","white"));
