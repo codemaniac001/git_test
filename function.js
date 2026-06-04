24276dc1dadff3a70030ed860b63a071aa9a4eb7
codemanic001 <workingnitesh1@gmail.com>
2026-06-04 21:14:08 +0530

    This is the some assignment one

---
+++ function.js
@@ -0,0 +1,79 @@
+// function sayhi()
+// {
+//     console.log("hello");
+// }
+// let func=sayhi;
+// func();
+// sayhi();
+//                             //This is the function one didnot work
+// function ask(question,yes,no)
+// {
+//     if(confirm(question)) yes()
+//         else
+//     no()
+// }
+// function showok()
+// {
+//     console.log("you agreed to it");
+// }
+// function showcancel()
+// {
+//     console.log("The show has been cancelled")
+// }
+// ask(question,showok,showcancel)
+
+// function sayhi(name){
+//     console.log(`hello,${name}`);
+// }
+// sayhi("john")
+//  let sayhi = function(name){
+//     console.log(`Hello,${name}`)
+//  };
+//  sayhi("john")
+                            //this is the another function part
+    // let age=prompt("are u",18)
+    // let welcome;
+    // if(age>18){
+    //     welcome=function(){
+    //         alert("welcome")
+    //     };
+    // }
+    // else{
+    //     welcome=function(){
+    //         alert("Not allowed, enter at your own risk");
+    //     };
+    // }
+    // welcome();
+                                    //write a function to add 7
+                                    
+            // function add7(num1){
+               
+            //    return 7  + num1
+            // }
+            // let num=Number(prompt("give a number for example",2));
+            // add7(num);                 //successfully done it
+        //  function multiply(num1,num2)
+        //  {
+        //     return(alert(num1*num2))
+        //  }
+        //  let num1=Number(prompt("give your first number"))
+        //  let num2=Number(prompt("give your second number"))
+        //  multiply(num1,num2)
+        // this too successfully done
+        /** this is the uppercase one */
+        // function capatilize(str){
+        //     return(alert(str.replace(str[0],str[0].toUpperCase())))
+        // }
+        // let str=prompt("give your sentence");
+        // capatilize(str)
+        /**Yes, yes i was freaking able to do it */
+           
+
+                            /**This is the last letter one! */
+        function lastletter(str){
+            let n= str.length;
+            alert(str[n-1])
+        }
+       str= prompt("enter the string")
+        lastletter(str)
+                           //this is also completed
