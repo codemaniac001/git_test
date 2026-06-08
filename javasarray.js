7cb33103380f9e3cfda761f4e1b5144cce865f8e
codemanic001 <workingnitesh1@gmail.com>
2026-06-08 20:17:02 +0530

    This is what i have learned in java till now

---
+++ javasarray.js
@@ -0,0 +1,17 @@
+                        //shift just starts deleting from first at first
+ let fruit1=["mango","watermelon","lichi","guava"]
+ fruit1.shift();
+ alert(fruit1)        
+                       // unshift just adds to the list  at first
+ let fruit2=["watermelon","litchi","guava"]
+ fruit2.unshift("strawberry")
+ alert(fruit2)
+                      //  we pop      from last
+  let fruit3=["watermelon","litchi","mango","guava"]
+  alert(fruit3.pop())
+  alert(fruit3)
+                      //  we push     at last
+     let fruit4=["watermelon","litchi","mango","guava"]
+     fruit4.push("water chestnut")
+     alert(fruit4)
+     
