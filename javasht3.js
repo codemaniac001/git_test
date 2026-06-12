fabf38b6397665979be75a286224bd38a5cae6fb
codemanic001 <workingnitesh1@gmail.com>
2026-06-12 07:45:08 +0530

    this is the next commit

---
+++ javasht3.js
@@ -0,0 +1,12 @@
+let home=document.querySelector('#home');
+home.addEventListener('click',(event)=>{
+    alert("Home has been clicked")
+});
+let dashboard=document.querySelector('#dashboard');
+dashboard.addEventListener('click',(event)=>{
+    alert("dashboard has been clicked")
+});
+let report=document.querySelector('#report');
+report.addEventListener('click',(event)=>{
+    alert("report has been clicked")
+});
